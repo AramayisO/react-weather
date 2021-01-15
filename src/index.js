89fb671a5e9d9d6b9b0060c72ae7c30cda2ddccd
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-01-14 23:49:44 -0800

    Render several WeatherSummary components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,54 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WeatherSummary from './WeatherSummary';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <WeatherSummary
+      dayOfWeek='Mon'
+      icon='https://picsum.photos/id/1/200/200'
+      highTemp={60} 
+      lowTemp={32} 
+    />
+
+    <WeatherSummary 
+      dayOfWeek='Tue' 
+      icon='https://picsum.photos/id/2/200/200' 
+      highTemp={60} 
+      lowTemp={32} 
+    />
+
+    <WeatherSummary 
+      dayOfWeek='Wed' 
+      icon='https://picsum.photos/id/3/200/200' 
+      highTemp={60} 
+      lowTemp={32} 
+    />
+
+    <WeatherSummary 
+      dayOfWeek='Thur' 
+      icon='https://picsum.photos/id/4/200/200' 
+      highTemp={60} 
+      lowTemp={32} 
+    />
+
+    <WeatherSummary 
+      dayOfWeek='Fri' 
+      icon='https://picsum.photos/id/5/200/200' 
+      highTemp={60} 
+      lowTemp={32} 
+    />
+  </>,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
