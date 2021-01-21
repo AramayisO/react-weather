33bfbecb20a3ec35c506ad2f40c64685361187c5
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-01-21 12:13:45 -0800

    Use conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { getWeatherData } from './utils';
+import WeatherDetails from './WeatherDetails';
+import WeatherSummary from './WeatherSummary';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      weather: null,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const { latitude, longitude } = position.coords;
+
+        try {
+          // If we successfully get data from the weather API, then we
+          // update our state with the new data that we got.
+          const weatherData = await getWeatherData(latitude, longitude);
+          this.setState({
+            weather: weatherData,
+            isLoading: false,
+          });
+        } catch(error) {
+          // If there's a problem getting data from the API, we want to inform
+          // the user that there was an error.
+          this.setState({
+            error: 'Oh no 😔 We were unable to get weather data at this time.',
+            isLoading: false
+          });
+        }
+
+      },
+      (error) => {
+        // If there's an error when we try to get the users current location,
+        // for example if the user blocks location services, then we want to
+        // show the user an error message.
+        this.setState({
+          error: 'Ooops 😓 We were unable to get your current location.',
+          isLoading: false
+        })
+      }
+    )
+  }
+
+  render() {
+
+    const { isLoading, weather, error } = this.state;
+
+    // Instead of doing nested ternary statements, I pulled this section out
+    // into a variable to make the code a little bit easier to read. The
+    // 'contets' variable just stores some JSX, and we can tell React to render
+    // that JSX by putting the variable in curly braces.
+    const content = (
+      <>
+        {error
+          ? <p>{error}</p>
+          : weather &&
+            <>
+              <WeatherDetails
+                  location="Fresno"
+                  dayOfWeek="Monday"
+                  weatherCondition="Partially cloudy"
+                  icon=""
+                  units="imperial"
+                  currentTemp={56}
+                  lowTemp={32}
+                  highTemp={60}
+                  precipitation={10} 
+                  humidity={30} 
+                  windSpeed={5}
+              />
+              {weather.daily.map((data, index) => (
+                <WeatherSummary
+                  key={data.dt}
+                  dayOfWeek='monday'
+                  icon={`https://picsum.photos/id/${index}/200/200`}
+                  highTemp={data.temp.max}
+                  lowTemp={data.temp.min}/>
+              ))}
+            </>
+        }
+      </>
+    )
+
+    return (
+      <div>
+        {isLoading
+          ? <p>Loading...⏳</p>
+          : <>{content}</>
+        }
+      </div>
+    )
+  }
+
+};
 
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,56 +1,15 @@
-import React, { createElement } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import WeatherSummary from './WeatherSummary';
 
 ReactDOM.render(
-  <>
-    <WeatherSummary
-      dayOfWeek='Mon'
-      icon='https://picsum.photos/id/1/200/200'
-      highTemp={56} 
-      lowTemp={34} 
-    />
-
-    <WeatherSummary 
-      dayOfWeek='Tue' 
-      icon='https://picsum.photos/id/2/200/200' 
-      highTemp={56} 
-      lowTemp={37} 
-    />
-
-    <WeatherSummary 
-      dayOfWeek='Wed' 
-      icon='https://picsum.photos/id/3/200/200' 
-      highTemp={58} 
-      lowTemp={36} 
-    />
-
-    <WeatherSummary 
-      dayOfWeek='Thur' 
-      icon='https://picsum.photos/id/4/200/200' 
-      highTemp={50} 
-      lowTemp={33} 
-    />
-
-    <WeatherSummary 
-      dayOfWeek='Fri' 
-      icon='https://picsum.photos/id/5/200/200' 
-      highTemp={48} 
-      lowTemp={32} 
-    />
-  </>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
